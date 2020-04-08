795589465880bec5648dc43d69b6565aaa2b3c01
alex9849 <alexander@liggesmeyer.de>
2020-04-08 17:39:04 +0200

    travis...

--- .travis/docker_deploy.sh
+++ .travis/docker_deploy.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-mvn versions:set -DnewVersion=$"TRAVIS_TAG"
-mvn versions:commit
-docker build -t alex9849/mc-pluginstats-server:$TRAVIS_TAG ./web
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push alex9849/mc-pluginstats-server
